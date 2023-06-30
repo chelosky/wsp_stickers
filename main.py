d19c6db188aea33d5981b8f692c630eeb2b5b8dc
Chelosky <marcelolazo19972013@hotmail.com>
2023-06-30 02:13:37 -0400

    :recycle: now the data file its generated for every pack and app pack

--- main.py
+++ main.py
@@ -10,29 +10,44 @@ JSON_FILE_NAME = 'data.json'
 folder_path = "{os_path}/{sticker_folder}".format(os_path=os.getcwd(), sticker_folder=STICKER_FOLDER)
 
 def generate_data():
-    sub_folders_paths = [{ 'path': os.path.join(folder_path, name), 'name': name } for name in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, name))]
+    app_packs = [{ 'path': os.path.join(folder_path, name), 'name': name } for name in os.listdir(folder_path) if os.path.isdir(os.path.join(folder_path, name))]
 
-    for sub_folder_path in sub_folders_paths:
-        unsorted_sticker_packs_paths =[{ 'path': os.path.join(sub_folder_path.get('path'), name), 'id': name } for name in os.listdir(sub_folder_path.get('path')) if os.path.isdir(os.path.join(sub_folder_path.get('path'), name))]
-        sticker_packs_paths = sorted(unsorted_sticker_packs_paths,key=lambda x: int(x.get('id')))
-        for sticker_pack_path in sticker_packs_paths:
-            base_sticker_url = '/'.join([BASE_URL, sub_folder_path.get('name'), sticker_pack_path.get('id')])
-            stickers = [ '/'.join([base_sticker_url, name]) for name in os.listdir(sticker_pack_path.get('path')) if name.split('.')[-1] == 'webp']
+    for app_pack in app_packs:
+        unsorted_sticker_packs =[{ 'path': os.path.join(app_pack.get('path'), name), 'id': name } for name in os.listdir(app_pack.get('path')) if os.path.isdir(os.path.join(app_pack.get('path'), name))]
+        sticker_packs = sorted(unsorted_sticker_packs,key=lambda x: int(x.get('id')))
+        
+        for sticker_pack in sticker_packs:
+            base_sticker_url = '/'.join([BASE_URL, app_pack.get('name'), sticker_pack.get('id')])
+            stickers = [ '/'.join([base_sticker_url, name]) for name in os.listdir(sticker_pack.get('path')) if name.split('.')[-1] == 'webp']
             icon = '/'.join([base_sticker_url, 'icon.png'])
-            sticker_pack_path['stickers'] = stickers
-            sticker_pack_path['icon'] = icon
-            sticker_pack_path['name'] = "{sticker_pack_name} PACK {pack_id}".format(sticker_pack_name=sub_folder_path.get('name').upper(), pack_id=sticker_pack_path.get('id'))
-            sticker_pack_path['id'] =  "{sticker_pack_name}_{pack_id}".format(sticker_pack_name=sub_folder_path.get('name'), pack_id=sticker_pack_path.get('id'))
-            del sticker_pack_path['path']
-        sub_folder_path['name'] = STICKERS_NAME.get(sub_folder_path.get('name'))
-        sub_folder_path['packs'] = sticker_packs_paths
-        del sub_folder_path['path']
-    return sub_folders_paths
+            sticker_pack['stickers'] = sorted(stickers, key=lambda x: int(x.split('/')[-1].split('.')[0]))
+            sticker_pack['icon'] = icon
+            sticker_pack['name'] = "{sticker_pack_name} PACK {pack_id}".format(sticker_pack_name=app_pack.get('name').upper(), pack_id=sticker_pack.get('id'))
+            sticker_pack['id'] =  int(sticker_pack.get('id'))
+            file_path = sticker_pack.get('path')
+            del sticker_pack['path']
+            generate_json_file(file_path, sticker_pack)
+        
+        app_pack['name'] = STICKERS_NAME.get(app_pack.get('name'))
+        original_sticker_packs = list(sticker_packs)
+        app_pack['packs'] = list(map(map_to_app_file, sticker_packs))
+        app_pack_path = app_pack.get('path')
+        del app_pack['path']
+        generate_json_file(app_pack_path, app_pack)
+        app_pack['packs'] = original_sticker_packs
+    
+    return app_packs
 
-def generate_json_file(data):
-    with open(JSON_FILE_NAME, "w") as outfile:
+def map_to_app_file(data):
+    MAX_STICKERS = 5
+    response = data.copy()
+    response['stickers'] = response.get('stickers')[0:MAX_STICKERS]
+    return response;
+
+def generate_json_file(path, data, file_name = JSON_FILE_NAME):
+    with open(os.path.join(path, file_name), "w") as outfile:
         outfile.write(json.dumps(data, indent=4))
 
 if __name__ == '__main__':
     response = generate_data()
-    generate_json_file(response)
+    generate_json_file('.', response)
